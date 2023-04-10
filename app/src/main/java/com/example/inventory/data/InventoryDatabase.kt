a7d16763f3f75fd9f614c2361f364eeb03a581dd
rib-bruno <ribeirobrunosouza@hotmail.com>
2023-04-10 18:00:28 -0300

    aplicativo finalizado - testes e room com flow

---
+++ app/src/main/java/com/example/inventory/data/InventoryDatabase.kt
@@ -0,0 +1,28 @@
+package com.example.inventory.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Item::class], version = 1, exportSchema = false)
+abstract class InventoryDatabase : RoomDatabase() {
+
+    abstract fun itemDao(): ItemDao
+
+    companion object {
+        @Volatile
+        //A variável Instance mantém uma referência ao banco de dados, quando uma tiver sido criada.
+        // Isso ajuda a manter uma única instância do banco de dados aberta em determinado momento, porque ela é um recurso com criação e manutenção caras.
+        private var Instance: InventoryDatabase? = null
+
+        fun getDatabase(context: Context): InventoryDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, InventoryDatabase::class.java, "item_database")
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/inventory/data/ItemDao.kt
@@ -0,0 +1,24 @@
+package com.example.inventory.data
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ItemDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(item: Item)
+
+    @Update
+    suspend fun update(item: Item)
+
+    @Delete
+    suspend fun delete(item: Item)
+
+    @Query("SELECT * from items WHERE id = :id") //extrair; :id usa a notação de dois-pontos na consulta para referenciar argumentos na função
+    fun getItem(id: Int) : Flow<Item>
+
+    @Query("SELECT * from items ORDER BY name ASC")
+    fun getAllItems(): Flow<List<Item>>
+
+}
